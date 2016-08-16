e21632115fd9c7fe81d3e67990df54c7cd808a75
Yogthos <dmitri.sotnikov@gmail.com>
2016-08-15 21:46:08 -0400

    cleanup

--- src/cljs/yuggoth/pages/home.cljs
+++ src/cljs/yuggoth/pages/home.cljs
@@ -1,10 +1,15 @@
 (ns yuggoth.pages.home
   (:require [reagent.core :as r]
             [re-frame.core :refer [subscribe]]
-            [yuggoth.bootstrap :as bs]))
+            [yuggoth.bootstrap :as bs]
+            [re-com.core
+             :refer [box v-box h-split v-split title flex-child-style input-text input-textarea]]))
 
 (defn home-page []
   [:div.container
    [:div.row
-    [:div.col-md-12
+    [:div.col-md-2
+     {:style {:border "solid"}}
+     [:h2 "Tags"]]
+    [:div.col-md-10
      [:h2 "TrackIt"]]]])
--- src/cljs/yuggoth/views.cljs
+++ src/cljs/yuggoth/views.cljs
@@ -31,7 +31,7 @@
        [bs/MenuItem {:on-click logout} "Logout"]])]])
 
 (defmulti pages identity)
-(defmethod pages :home [] [edit-issue-page] #_[home-page])
+(defmethod pages :home [] [home-page])
 (defmethod pages :login [] [login-page])
 (defmethod pages :edit-issue [] [edit-issue-page])
 (defmethod pages :default [] [:div])
