cf4fa0f0003ec85a7e9ce8e0aba5941ab53321ac
Sotnikov, Dmitri <dmitri.sotnikov@gmail.com>
2016-08-23 17:25:07 -0400

    fixed event handling on login

--- src/cljs/memory_hole/handlers.cljs
+++ src/cljs/memory_hole/handlers.cljs
@@ -17,6 +17,7 @@
 (reg-event-db
   :run-login-events
   (fn [db _]
+    (println "running login events:" (:login-events db))
     (doseq [event (:login-events db)]
       (dispatch event))
     db))
