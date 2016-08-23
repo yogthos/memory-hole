dd2427f30ed20ca48655aab9c404d0ee78d8ed00
Sotnikov, Dmitri <dmitri.sotnikov@gmail.com>
2016-08-23 16:42:46 -0400

    renamed namespaces

--- project.clj
+++ project.clj
@@ -95,7 +95,7 @@
                                  [doo "0.1.7"]
                                  [binaryage/devtools "0.8.1"]
                                  [figwheel-sidecar "0.5.4-7"]
-                                 [com.cemerick/piggieback "0.2.2-SNAPSHOT"]]
+                                 [com.cemerick/piggieback "0.2.1"]]
                   :plugins      [[com.jakemccrary/lein-test-refresh "0.14.0"]
                                  [lein-doo "0.1.7"]
                                  [lein-figwheel "0.5.4-7"]
