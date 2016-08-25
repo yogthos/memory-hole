021a9005fe1eeab9ffadd278bc0be66a0f85399e
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-08-25 09:44:23 -0400

    added support for local logins

--- resources/migrations/00000000000100-users.up.sql
+++ resources/migrations/00000000000100-users.up.sql
@@ -2,6 +2,7 @@ CREATE EXTENSION IF NOT EXISTS citext WITH SCHEMA public;
 CREATE TABLE users
 (user_id     SERIAL      NOT NULL PRIMARY KEY,
  screenname  VARCHAR(30) UNIQUE NOT NULL,
+ pass        TEXT,
  email       VARCHAR(60),
  admin       BOOLEAN,
  last_login  TIMESTAMP,
@@ -10,5 +11,5 @@ CREATE TABLE users
  update_date TIMESTAMP   NOT NULL DEFAULT (now() AT TIME ZONE 'utc'));
 --;;
 -- create a system account for which the stock entries will be created
-INSERT INTO users (screenname, email, admin, is_active)
-VALUES ('Admin', 'admin@admin.com', TRUE, TRUE);
+INSERT INTO users (screenname, email, admin, is_active, last_login, pass)
+VALUES ('admin', 'admin@admin.com', TRUE, TRUE, (select now() at time zone 'utc'), 'bcrypt+sha512$86186fc28f83b3e3db78bcf8350a3a57$12$8f215420e68fd7922561167b07354f05d8db6d49e212689e');
--- resources/sql/users.sql
+++ resources/sql/users.sql
@@ -1,19 +1,20 @@
 -- :name insert-user<! :<! :1
 -- :doc add a user.
-insert into users (screenname, admin, last_login, is_active)
-     values (:screenname, :admin, (select now() at time zone 'utc'), :is-active)
+insert into users (screenname, admin, last_login, is_active, pass)
+     values (:screenname, :admin, (select now() at time zone 'utc'), :is-active, :pass)
 returning user_id;
 
 -- :name user-by-screenname :? :1
 -- :doc get a user based on the id.
-select user_id, screenname, admin, last_login, is_active
+select user_id, screenname, admin, last_login, is_active, pass
 from users
 where screenname = :screenname;
 
 -- :name update-user<! :<! :1
 -- :doc Updates the last login date and screenname for a user.
 update users
-set screenname =:screenname,
+set screenname = :screenname,
+    pass = :pass,
     last_login=(select now() at time zone 'utc')
 where user_id=:user-id
 returning user_id, is_active, admin;
