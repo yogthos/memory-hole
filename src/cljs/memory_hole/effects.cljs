50c650cb51fdc6589f280d9041d5d47e7b6e95c7
Scot Brown <scotb38@gmail.com>
2017-11-16 18:16:46 -0500

    fix missing unset loading event dispatch when no success event passed

--- src/cljs/memory_hole/effects.cljs
+++ src/cljs/memory_hole/effects.cljs
@@ -13,15 +13,16 @@
        :or {error-event [:ajax-error]
             ajax-map {}}}]
    (dispatch [:set-loading])
-   (method url (merge (when success-event
-                        {:handler (fn [response]
-                                    (dispatch (if ignore-response-body
-                                                success-event
-                                                (conj success-event response)))
-                                    (dispatch [:unset-loading]))})
-                      {:error-handler (fn [error]
-                                        (dispatch (conj error-event error))
-                                        (dispatch [:unset-loading]))}
+   (method url (merge
+                {:handler (fn [response]
+                            (when success-event
+                              (dispatch (if ignore-response-body
+                                          success-event
+                                          (conj success-event response))))
+                            (dispatch [:unset-loading]))
+                 :error-handler (fn [error]
+                                  (dispatch (conj error-event error))
+                                  (dispatch [:unset-loading]))}
                       ajax-map))))
 (defn context-url [url]
   (str js/context url))
